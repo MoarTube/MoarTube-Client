171780ea87240cc0cd8b61dd4eeabc18f3d0f8b7
Chris <cconley717@gmail.com>
2024-02-16 09:10:15 -0600

    removed Electron dependencies

--- main.js
+++ main.js
@@ -1,3 +1,54 @@
+/*
+no longer being used, but being kept in the event Electron is used again
+
+{
+  "name": "moartube-client",
+  "version": "1.0.16",
+  "description": "A free, open-source, self-hosted, anonymous, decentralized video/live stream platform. Scalable via Cloudflare, works in the cloud or from home WiFi.",
+  "author": "MoarTube, LLC",
+  "license": "Custom License - See LICENSE.md file",
+  "main": "moartube-client.js",
+  "scripts": {
+    "start": "electron-forge start",
+    "startApp": "electron main.js",
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "package": "electron-forge package",
+    "make": "electron-forge make",
+    "publish": "electron-forge publish"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/MoarTube/MoarTube-Client.git"
+  },
+  "keywords": ["alternative", "youtube", "twitch", "video", "stream", "live", "moartube"],
+  "dependencies": {
+    "axios": "^1.4.0",
+    "body-parser": "^1.20.2",
+    "cors": "^2.8.5",
+    "electron-squirrel-startup": "^1.0.0",
+    "express": "^4.18.2",
+    "express-session": "^1.17.3",
+    "ffmpeg-static": "^5.2.0",
+    "form-data": "^4.0.0",
+    "multer": "^1.4.5-lts.1",
+    "portscanner": "^2.2.0",
+    "sharp": "^0.33.0",
+    "systeminformation": "^5.18.14",
+    "ws": "^8.13.0"
+  },
+  "devDependencies": {
+    "@electron-forge/cli": "^7.2.0",
+    "@electron-forge/maker-deb": "^7.2.0",
+    "@electron-forge/maker-rpm": "^7.2.0",
+    "@electron-forge/maker-squirrel": "^7.2.0",
+    "@electron-forge/maker-zip": "^7.2.0",
+    "@electron-forge/plugin-auto-unpack-natives": "^7.2.0",
+    "@electron-forge/publisher-github": "^7.2.0",
+    "electron": "^28.0.0"
+  }
+}
+*/
+
 const { app, BrowserWindow } = require("electron");
 
 global.electronPaths = {
